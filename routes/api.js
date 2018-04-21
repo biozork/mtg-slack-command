97f91e238a58aa38944079226ef8c4061aa79196
biozork <larsemil@biozork.dk>
2018-04-22 00:17:53 +0200

    asd

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
-console.log(ding);
+console.log("ding");
 app.post('/', bodyParser.json(), function (req, res, next) {
 	console.log(req.body);
 
