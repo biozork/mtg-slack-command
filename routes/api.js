d0738f2a0c83daab3cb1a65b13f29c3d80daecd9
biozork <larsemil@biozork.dk>
2018-04-22 01:07:28 +0200

    does server break ?

--- routes/api.js
+++ routes/api.js
@@ -5,9 +5,7 @@ const magicthegathering = require('../services/magicthegathering.io.js');
 var app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', function (req, res, next) {
 	console.log(req.body);
@@ -38,31 +36,8 @@ app.post('/', function (req, res, next) {
 
 });
 
-app.post('/interactive', function (req, res, next) {
-	console.log(req.body);
-
-	if (req.body.command == "/card" && req.body.token == process.env.SLACK_KEY) {
-		magicthegathering.card(req.body.text, function (card) {
-			res.setHeader('Content-Type', 'application/json');
-			res.end(JSON.stringify(card, null, 4))
-		})
-
-	} else {
-
-		if (req.body.token != process.env.SLACK_KEY) {
-			console.log('Server token: ' + process.env.SLACK_KEY);
-			console.log('Request token: ' + req.body.token);
-			
-			res.end(JSON.stringify({
-				"Error": "Your app is not authorized to access this service"
-			}))
-		} else {
-			res.end(JSON.stringify({
-				"Error": "Command not recognized"
-			}))
-		}
-
-	}
+app.post('/interactive', bodyParser.json(), function (req, res, next) {
+	let payload = JSON.parse(req.body.payload);
 
 });
 
